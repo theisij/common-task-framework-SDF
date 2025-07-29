ae8037a9cdb5df9f69047cf3973ef9012e6039ad
Oliver Hellum <oliver.hellum@gmail.com>
2025-07-29 13:14:34 +0200

    Updated KNS

--- models_R/KNS/kns_pc.R
+++ models_R/KNS/kns_pc.R
@@ -1,9 +1,22 @@
-rank_weighted_factor_ws <- function(chars, features) {
-  factor_ws <- copy(chars)
-  factor_ws[, (features) := lapply(.SD, function(r) (r - mean(r)) / sum(abs(r - mean(r)))), .SDcols=features, by = eom]
-  factor_ws[, (features) := lapply(.SD, function(w) ifelse(is.na(w), 0, w)), .SDcols=features]
+prepare_data <- function(chars, features) {
+  chars[, (features) := lapply(.SD, as.double), .SDcols = features]  # Convert feature columns to double to avoid loosing precision 
+  for(f in features) {
+    if (match(f, features) %% 10 == 0) print(paste0("Feature ", match(f, features), " out of ", length(features)))
+    chars[, zero := (get(f) == 0)]
+    chars[!is.na(get(f)), (f) := ecdf(get(f))(get(f)), by = eom]
+    chars[zero == T, (f) := 0][, zero := NULL]  # Set exact zeros to 0 (ecdf always returns >0)
+  }
+  # Feature Imputation 
+  chars[, (features) := lapply(.SD, function(x) if_else(is.na(x), 0.5, x)), .SDcols=features, by=eom]
 
-  factor_ws |> 
+  chars[, (features) := lapply(.SD, function(r) (r - mean(r)) / sum(abs(r - mean(r)))), .SDcols=features, by = eom]
+  chars[, (features) := lapply(.SD, function(w) ifelse(is.na(w), 0, w)), .SDcols=features]
+  chars[, me := me / sum(me), by = eom]
+  chars
+}
+
+rank_weighted_factor_ws <- function(chars, features) {
+  chars |> 
     select(id, eom, eom_ret, all_of(features)) |>
     melt(
       id.vars = c("id", "eom", "eom_ret"),
@@ -198,7 +211,7 @@ calc_opt_lambdas <- function(d, rw_factor_d_raw, mkt_ret_daily, features, n_fold
   )
 }
 
-kns <- function(d, rw_factor_d_raw, rw_factor_m, mkt_ret_daily, mkt_ret, features, opt_lambdas, use_pc) {
+kns <- function(d, rw_factor_d_raw, mkt_ret_daily, features, opt_lambdas, use_pc) {
     l1 <- opt_lambdas[eom_ret == d, l1]
     l2 <- opt_lambdas[eom_ret == d, l2]
     hist <- rw_factor_d_raw |> filter(eom_ret < d, eom_ret >= d + 1 - months(120) - 1)
@@ -232,16 +245,17 @@ kns <- function(d, rw_factor_d_raw, rw_factor_m, mkt_ret_daily, mkt_ret, feature
       t(w[-length(w)]),
       me = sum(t(w) * c(-beta, 1))
     )
-    names(dt) <- c("eom_ret", "ret_exc", "name", features, "me")
+    names(dt) <- c("eom_ret", "name", features, "me")
     dt
 }
 
 
 main <- function(chars, features, daily_ret) {
-  rw_factors <- create_rw_factors(chars, daily_ret, features)
-  mkt_rets <- create_mkt_rets(chars, daily_ret)
+  chars_normalized <- prepare_data(chars, features)
+  rw_factors <- create_rw_factors(chars_normalized, daily_ret, features)
+  mkt_rets <- create_mkt_rets(chars_normalized, daily_ret)
 
-  pf_dates <- unique(subset(chars, ctff_test == TRUE)$eom_ret)
+  pf_dates <- unique(subset(chars_normalized, ctff_test == TRUE)$eom_ret)
 
   l2s <- 10^seq(-10, 0, 0.1)
   l1s <- 10^seq(-10, 4, 0.1)
@@ -249,10 +263,10 @@ main <- function(chars, features, daily_ret) {
 
   plan(multisession, workers = 8)
   options(future.globals.maxSize = 5000*1024^2, future.rng.onMisuse = "ignore")
-  opt_lambdas_pc <- pf_dates |>
+  opt_lambdas <- pf_dates |>
     # map(.progress = TRUE, function(d) {
     future_map(.progress = TRUE, function(d) {
-      calc_opt_lambdas(d, rw_factor["daily"], mkt_rets["daily"], features, n_folds, l2s, l1s, use_pc=TRUE)
+      calc_opt_lambdas(d, rw_factor$daily, mkt_rets$daily, features, n_folds, l2s, l1s, use_pc=TRUE)
     # }) |>
     }, .options = furrr_options(packages = c("data.table", "tidyverse"))) |>
     rbindlist() |>
@@ -261,22 +275,20 @@ main <- function(chars, features, daily_ret) {
   w_kns_pc <- pf_dates |>
     # map(.progress = TRUE, function(d) {
     future_map(.progress = TRUE, function(d) {
-      kns(d, rw_factor["daily"], mkt_rets["daily"], features, opt_lambdas, use_pc=TRUE)
+      kns(d, rw_factor$daily, mkt_rets$daily, features, opt_lambdas, use_pc=TRUE)
     # }) |>
     }, .options = furrr_options(packages = c("data.table", "tidyverse"))) |>
     rbindlist() |>
     setDT()
 
-  test_chars <- chars |> filter(ctff_test == TRUE)
-  test_chars[, (features) := lapply(.SD, function(r) (r - mean(r)) / sum(abs(r - mean(r)))), .SDcols=features, by = eom]
-  test_chars[, (features) := lapply(.SD, function(w) ifelse(is.na(w), 0, w)), .SDcols=features]
-  test_chars[, me := me / sum(me), by = eom]
-  test_chars <- test_chars |> select(id, eom, me, all_of(features))
+  test_chars <- chars_normalized |> 
+    filter(ctff_test == TRUE) |>
+    select(id, eom, eom_ret, me, all_of(features))
 
-  opt_chars <- unique(test_chars$eom) |>
+  pf <- unique(test_chars$eom_ret) |>
     map(.progress = TRUE, function(d) {
-      test_chars_d <- test_chars |> filter(eom_ret)
-      w_kns_pc_d <- w_kns_pc |> filter(eom_ret)
+      test_chars_d <- test_chars |> filter(eom_ret == d)
+      w_kns_pc_d <- w_kns_pc |> filter(eom_ret == d)
 
       for (f in c(features, "me")) {
         test_chars_d[[f]] <- test_chars_d[[f]] * w_kns_pc_d[[f]]
@@ -286,8 +298,32 @@ main <- function(chars, features, daily_ret) {
     rbindlist() |>
     setDT()
 
-  opt_chars$w <- opt_chars |> select(all_of(features), "me") |> rowSums()
+  pf$w <- pf |> select(all_of(features), "me") |> rowSums()
 
   # Output
-  opt_chars[, .(id, eom, w)]
+  pf[, .(id, eom, w)]
+}
+
+
+# Download the input data
+features <- read_parquet("../Data/CTF-data/ctff_features.parquet") |> pull(features)
+chars <- read_parquet("../Data/CTF-data/ctff_chars.parquet")
+daily_ret <- read_parquet("../Data/CTF-data/ctff_daily_ret.parquet") # Users don't have to use this data
+# Run code
+pf <- chars |> main(daily_ret=daily_ret, features=features)
+# Save output
+pf |> fwrite("../Data/CTF-output/kns_portfolio.csv")
+
+# On our end --------------------------------------------------------------------
+# We source submitted code above
+# Save the outputted portfolio
+# Take the output in a separate file and visualize the performance:
+if (FALSE) {
+  chars[ctff_test==T, .(id, eom, eom_ret, ret_exc_lead1m)][pf, on = .(id, eom)][, .(
+    ret = sum(w * ret_exc_lead1m)
+  ), by = eom_ret][, .(
+    mean = mean(ret)*12,
+    sd = sd(ret)*sqrt(12),
+    sr = mean(ret)/sd(ret)*sqrt(12)
+  )]
 }
