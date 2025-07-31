959228362ae0a6f6b422dee283a39f3cd19904e7
theisij <70517574+theisij@users.noreply.github.com>
2025-07-31 15:28:10 +0200

    Added data preparation utility
    * Impute and rank data preparation

---
+++ scripts/data_prep.py
@@ -0,0 +1,32 @@
+import time
+
+import polars as pl
+
+from utils.data_prep import impute_and_rank
+
+# Prepare input
+features = (
+    pl
+    .read_parquet("data/raw/ctff_features.parquet")
+    .get_column("features")
+    .to_list()
+)
+df = pl.scan_parquet("data/raw/ctff_chars.parquet") # Change name from df to chars
+ids = ["id", "excntry", "eom", "eom_ret", "ret_exc_lead1m", "ctff_test"]
+group_cols = ["excntry", "eom", "feature"]
+
+# Impute and rank
+start_time = time.perf_counter()
+data_processed = impute_and_rank(
+    df=df,
+    ids=ids,
+    features=features,
+    feat_prank=True,  
+    impute_flag=True,  
+    group_cols=group_cols
+)
+print(f"Elapsed time: {time.perf_counter()-start_time:.6f} seconds") # 1000 sec for lazy API without streaming
+
+# Save 
+data_processed.write_parquet("data/processed/ctff_chars_processed.parquet")
+
---
+++ utils/data_prep.py
@@ -0,0 +1,81 @@
+import polars as pl
+
+
+def impute_and_rank(
+    df: pl.LazyFrame | pl.DataFrame,
+    ids: list[str],
+    features: list[str],
+    feat_prank: bool,
+    impute_flag: bool,
+    group_cols: list[str]
+) -> pl.DataFrame:
+    """
+    Master function to prepare prediction data.
+    1. Apply percentile ranks if feat_prank is True
+    2. Impute missing values based on feat_prank flag
+    """
+    # Pivot data to long format (and make sure that value is numeric)
+    df = (
+        df
+        .select(ids + features)  
+        .unpivot(
+            index = ids,        
+            on = features,  
+            variable_name = "feature", 
+            value_name = "value"
+        )
+        .cast({"value": pl.Float64})
+    )
+
+
+    # Percentile rank
+    if feat_prank:
+        df = (
+                df
+                .with_columns(
+                    zeroes = pl.col('value') == 0,
+                    prank_raw = (
+                        pl.col('value')
+                        .rank(method="max") # R uses "max" for ECDF, but maybe min is better?
+                        .over(group_cols) 
+                    ) / (
+                        pl.col('value')
+                        .count()
+                        .over(group_cols)
+                    )
+                )
+                .with_columns(
+                    # Set exact zeros to zero (value always return positive ranks)
+                    value = pl.when(pl.col('zeroes')).then(0).otherwise(pl.col('prank_raw'))
+                )
+                .drop("zeroes", "prank_raw")
+            )
+        # Center percentile ranks centered around 0
+        df = (
+            df
+            .with_columns(
+                value = pl.col("value") - 0.5
+            )
+        )
+    
+    # Imputation
+    if impute_flag and feat_prank:
+        df = df.fill_null(0)
+
+    if impute_flag and not feat_prank:
+        df = df.fill_null(
+                value = pl.col('value').median().over(group_cols)
+            )
+
+    # # Cast to wide format
+    df = df.collect() # Pivot doesn't work with LazyFrame
+    df = (
+        df
+        .pivot(
+            index = ids,
+            on = "feature",
+            values = "value"
+        )
+    )
+
+    return df
